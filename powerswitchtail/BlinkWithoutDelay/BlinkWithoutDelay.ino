7742307b1189d2de19893469c48804c17adc9c2f
zivschneider <ziv.schneider@gmail.com>
2015-03-21 17:38:19 -0400

    powerswitch tail arduino code

---
+++ powerswitchtail/BlinkWithoutDelay/BlinkWithoutDelay.ino
@@ -0,0 +1,38 @@
+
+const int powerPin = A0;
+boolean powerOn = false;
+unsigned long previousMillis = 0;        // will store last time LED was updated
+
+// constants won't change :
+const long interval = 40000;           // interval at which to blink (milliseconds)
+
+void setup() {
+  
+  Serial.begin(9600);
+  Serial.println(F("starting up"));
+  pinMode(powerPin, OUTPUT);
+  
+}
+
+void loop()
+{
+  
+  unsigned long currentMillis = millis();
+ 
+  if(currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED 
+    previousMillis = currentMillis;   
+ 
+    powerOn = !powerOn;
+    
+    if (powerOn) {
+      digitalWrite(powerPin, LOW);
+      Serial.println(F("ON")); } 
+      else {
+      digitalWrite(powerPin, HIGH);
+      Serial.println(F("OFF"));
+    }    
+
+  }
+}
+
